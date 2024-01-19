48bc61e07325660032297ecbd1065ba27883293d
Yevhenns <gifestum@gmail.com>
2024-01-19 16:51:34 +0200

    extra items

--- src/@data/data.ts
+++ src/@data/data.ts
@@ -139,4 +139,144 @@ export const data = [
       },
     ],
   },
+  {
+    accountId: "a3",
+    email: "email3@mail.com",
+    authToken: "asdqwe",
+    creationDate: "12.03.2018",
+    profiles: [
+      {
+        profileId: "p1-1",
+        country: "Ukraine",
+        marketplace: "prom.ua",
+        campaigns: [
+          {
+            campaignId: "c1-1",
+            clicks: 12,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c2-1",
+            clicks: 24,
+            cost: 370,
+            date: "25.12.2018",
+          },
+          {
+            campaignId: "c3-1",
+            clicks: 22,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c4-1",
+            clicks: 54,
+            cost: 310,
+            date: "25.12.2018",
+          },
+        ],
+      },
+      {
+        profileId: "p2-1",
+        country: "Ukraine",
+        marketplace: "olx",
+        campaigns: [
+          {
+            campaignId: "c1-1",
+            clicks: 122,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c2-1",
+            clicks: 243,
+            cost: 370,
+            date: "25.12.2018",
+          },
+          {
+            campaignId: "c3-1",
+            clicks: 224,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c4-1",
+            clicks: 541,
+            cost: 310,
+            date: "25.12.2018",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    accountId: "a4",
+    email: "email4@mail.com",
+    authToken: "asdqwe",
+    creationDate: "12.03.2019",
+    profiles: [
+      {
+        profileId: "p1-2",
+        country: "Poland",
+        marketplace: "Allegro",
+        campaigns: [
+          {
+            campaignId: "c1-2",
+            clicks: 212,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c2-2",
+            clicks: 724,
+            cost: 370,
+            date: "25.12.2018",
+          },
+          {
+            campaignId: "c3-2",
+            clicks: 422,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c4-2",
+            clicks: 154,
+            cost: 310,
+            date: "25.12.2018",
+          },
+        ],
+      },
+      {
+        profileId: "p2-2",
+        country: "USA",
+        marketplace: "Amazon",
+        campaigns: [
+          {
+            campaignId: "c1-2",
+            clicks: 129,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c2-2",
+            clicks: 249,
+            cost: 370,
+            date: "25.12.2018",
+          },
+          {
+            campaignId: "c3-2",
+            clicks: 229,
+            cost: 300,
+            date: "14.02.2018",
+          },
+          {
+            campaignId: "c4-2",
+            clicks: 549,
+            cost: 310,
+            date: "25.12.2018",
+          },
+        ],
+      },
+    ],
+  },
 ];
